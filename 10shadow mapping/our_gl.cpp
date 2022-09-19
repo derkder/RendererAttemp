f93f6e544367fa56a170967beccf36ade4ba6263
xuhaoyi <xuhaoyi.250@bytedance.com>
2022-09-19 18:52:37 +0800

    光线追踪画多个球体加光照

---
+++ 10shadow mapping/our_gl.cpp
@@ -0,0 +1,118 @@
+#include <cmath>
+#include <limits>
+#include <cstdlib>
+#include "our_gl.h"
+#include <algorithm>
+Matrix ModelView;
+Matrix Viewport;
+Matrix Projection;
+
+IShader::~IShader() {}
+
+//视角矩阵mvp中的view
+void viewport(int x, int y, int w, int h) {
+    Viewport = Matrix::identity();
+    Viewport[0][3] = x+w/2.f;
+    Viewport[1][3] = y+h/2.f;
+    Viewport[2][3] = 255.f/2.f;
+    Viewport[0][0] = w/2.f;
+    Viewport[1][1] = h/2.f;
+    Viewport[2][2] = 255.f/2.f;
+}
+
+//投影矩阵mvp中的projection
+//param:coeff = -1.f/(eye-center)
+void projection(float coeff) {
+    Projection = Matrix::identity();
+    Projection[3][2] = coeff;
+}
+
+//变换矩阵mvp中的model
+void lookat(Vec3f eye, Vec3f center, Vec3f up) {
+    Vec3f z = (eye-center).normalize();
+    Vec3f x = cross(up,z).normalize();
+    Vec3f y = cross(z,x).normalize();
+    ModelView = Matrix::identity();
+    Matrix translaition = Matrix::identity();
+	Matrix rotation     = Matrix::identity();
+	for (int i = 0; i < 3; i++) {
+        translaition[i][3] = -center[i];
+	}
+    for (int i=0; i<3; i++) {
+        rotation[0][i] = x[i];
+        rotation[1][i] = y[i];
+        rotation[2][i] = z[i];
+    }
+    ModelView = rotation* translaition;
+}
+
+//计算质心坐标
+//只是计算，并没有判断是否在三角形里
+Vec3f barycentric(Vec2f A, Vec2f B, Vec2f C, Vec2f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = B[i] - A[i];
+        s[i][1] = C[i] - A[i];
+        s[i][2] = A[i]-P[i];
+    }
+    Vec3f res = cross(s[0], s[1]);
+    ////res[2]=0代表三点共线
+    if (std::abs(res[2])>1e-2) 
+        return Vec3f(1.f-(res.x+res.y)/res.z, res.x/res.z, res.y/res.z);
+    return Vec3f(-1,1,1);
+}
+
+//绘制三角形
+void triangle(Vec4f *pts, IShader &shader, TGAImage &image, TGAImage &zbuffer) {
+    //初始化三角形边界框
+    Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            //这里pts除以了最后一个分量，实现了透视中的缩放，所以作为边界框
+            //pts[i]为三角形的三个顶点
+            //pts[i][2]为三角形的z信息(0~255)
+            //pts[i][3]为三角形的投影系数(1-z/c)
+            bboxmin[j] = std::min(bboxmin[j], pts[i][j]/pts[i][3]);
+            bboxmax[j] = std::max(bboxmax[j], pts[i][j]/pts[i][3]);
+        }
+    }
+    //当前像素坐标P，颜色color
+    Vec2i P;
+    TGAColor color;
+    //遍历边界框中的每一个像素
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            //c为当前P对应的质心坐标
+            //这里pts除以了最后一个分量，实现了透视中的缩放，所以用于判断P是否在三角形内
+            Vec3f c = barycentric(proj<2>(pts[0]/pts[0][3]), proj<2>(pts[1]/pts[1][3]), proj<2>(pts[2]/pts[2][3]), proj<2>(P));
+            //插值计算P的zbuffer
+            float z_P = (pts[0][2]/ pts[0][3])*c.x + (pts[0][2] / pts[1][3]) *c.y + (pts[0][2] / pts[2][3]) *c.z;
+            int frag_depth = std::max(0, std::min(255, int(z_P+.5)));
+            //P的任一质心分量小于0或者zbuffer小于已有zbuffer，不渲染
+            //正常来说如果是深度测试，应该是值越小代表越在前面，但这里用z的大小代表的深度，也就是越大越在前面
+            if (c.x<0 || c.y<0 || c.z<0 || zbuffer.get(P.x, P.y)[0]>frag_depth) continue;
+            Vec3f c_revised = { 0,0,0 };
+            for (int i = 0; i < 3; ++i)
+            {
+                //求α，β，γ,只需要除以pts第四个分量即可
+                c_revised[i] = c[i] / pts[i][3];
+            }
+            float Z_n = 1. / (c_revised[0] + c_revised[1] + c_revised[2]);
+            for (int i = 0; i < 3; ++i)
+            {
+                //求正确透视下插值的系数
+                c_revised[i] *= Z_n;
+            }
+            //调用片元着色器计算当前像素颜色，第二个参数是个地址，fragment会给这个赋值
+            bool discard = shader.fragment(c_revised, color);
+            if (!discard) {
+                //zbuffer
+                zbuffer.set(P.x, P.y, TGAColor(frag_depth));
+                //为像素设置颜色
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
+}
+
---
+++ 附件：光线追踪/RayTracer/RatTracer/RatTracer.cpp
@@ -0,0 +1,130 @@
+﻿#include "geometry.h"
+#include <limits>
+#include <fstream>
+#include <vector>
+#define M_PI       3.14159265358979323846   // pi
+
+struct Light
+{
+    Light(const vec3& p, const float& i) : position(p), intensity(i) {}
+    vec3 position;
+    float intensity;
+};
+
+struct Material
+{
+    Material(const vec3& color) : diffuse_color(color) {}
+    Material() : diffuse_color() {}
+    vec3 diffuse_color;//表示物体自身颜色的参数
+};
+
+struct Sphere
+{
+    vec3 center;
+    float radius;
+    Material material;
+    Sphere(const vec3& c, const float& r, const Material& m) : center(c), radius(r), material(m) {}
+    bool ray_intersect(const vec3& orig, const vec3& dir, float& t0) const
+    {
+        vec3 L = center - orig;
+        float tca = L * dir;
+        float d2 = L * L - tca * tca;
+        if (d2 > radius * radius)
+            return false;
+        float thc = sqrtf(radius * radius - d2);
+        t0 = tca - thc;
+        float t1 = tca + thc;
+        if (t0 < 0) t0 = t1;//说明光源的出发点在球体内部
+        if (t0 < 0) return false;
+        return true;
+    }
+};
+
+bool scene_intersect(const vec3& orig, const vec3& dir, const std::vector<Sphere>& spheres, vec3& hit, vec3& N, Material& material)//每个屏幕上的像素点跑一个
+{
+    float spheres_dist = std::numeric_limits<float>::max();//用于存储当前光线与场景中的球体最早相交的时间（这里时间等同于距离）
+    for (size_t i = 0; i < spheres.size(); i++)//逐个球看有没有相交
+    {
+        float dist_i;
+        if (spheres[i].ray_intersect(orig, dir, dist_i) && dist_i < spheres_dist)//有离摄像头更近的球了
+        {
+            spheres_dist = dist_i;
+            //因为传入进来的是地址
+            hit = orig + dir * dist_i;
+            N = (hit - spheres[i].center).normalize();
+            material = spheres[i].material;
+        }
+    }
+    return spheres_dist < 1000;
+}
+
+vec3 cast_ray(const vec3& orig, const vec3& dir, const std::vector<Sphere>& spheres, const std::vector<Light>& lights)//每个屏幕上的像素点跑一个
+{
+    vec3 point, N;
+    Material material;
+
+    if (!scene_intersect(orig, dir, spheres, point, N, material))
+    {
+        return vec3{ 0.2, 0.7, 0.8 }; // background color
+    }
+
+    float diffuse_light_intensity = 0;
+    for (size_t i = 0; i < lights.size(); i++)
+    {
+        vec3 light_dir = (lights[i].position - point).normalize();
+        diffuse_light_intensity += lights[i].intensity * std::max(0.f, light_dir * N);
+    }
+    return material.diffuse_color * diffuse_light_intensity;
+}
+
+
+void render(const std::vector<Sphere>& spheres, const std::vector<Light>& lights)
+{
+    const int width = 1024;
+    const int height = 768;
+    const float fov = M_PI / 3.0;
+    std::vector<vec3> framebuffer(width * height);
+    for (size_t j = 0; j < height; j++)
+    {
+        for (size_t i = 0; i < width; i++)
+        {
+            //获得像素【一个平面上的】的世界坐标
+            float x = (i + 0.5) - width / 2.;
+            float y = -(j + 0.5) + height / 2.;
+            float z = -height / (2. * tan(fov / 2.));//固定的，因为是在一个屏幕上
+            vec3 dir = vec3{ x, y, z }.normalize();
+            framebuffer[i + j * width] = cast_ray(vec3{ 0, 0, 0 }, dir, spheres, lights);
+        }
+    }
+    std::ofstream ofs;
+    ofs.open("./outPureSphereImage.ppm", std::ios::binary);
+    ofs << "P6\n"
+        << width << " " << height << "\n255\n";
+    for (size_t i = 0; i < height * width; ++i)
+    {
+        for (size_t j = 0; j < 3; j++)
+        {
+            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
+        }
+    }
+    ofs.close();
+}
+
+
+int main()
+{
+    Material purpel_material(vec3{ 0.58, 0.44, 0.86 });
+    Material red_material(vec3{ 1.0, 0.42, 0.42 });
+
+    std::vector<Sphere> spheres;
+    spheres.push_back(Sphere(vec3{ -3, 0, -16 }, 2, purpel_material));
+    spheres.push_back(Sphere(vec3{ -1.0, -1.5, -12 }, 2, red_material));
+    spheres.push_back(Sphere(vec3{ 1.5, -0.5, -18 }, 3, red_material));
+    spheres.push_back(Sphere(vec3{ 7, 5, -18 }, 4, purpel_material));
+
+    std::vector<Light> lights;
+    lights.push_back(Light(vec3{ -20, 20, 20 }, 1.5));
+
+    render(spheres, lights);
+    return 0;
+}
